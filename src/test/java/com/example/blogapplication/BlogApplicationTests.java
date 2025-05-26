3995737671e75474ad1aaeb1b454c841b067a8ba
MANPRIT SINGH <83352826+manpritsingh-mod@users.noreply.github.com>
2025-05-26 11:50:55 +0530

    Update BlogApplicationTests.java

--- src/test/java/com/example/blogapplication/BlogApplicationTests.java
+++ src/test/java/com/example/blogapplication/BlogApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.blogapplication;
+// package com.example.blogapplication;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BlogApplicationTests {
+// @SpringBootTest
+// class BlogApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//     // @Test
+//     // void contextLoads() {
+//     // }
 
-}
+// }
